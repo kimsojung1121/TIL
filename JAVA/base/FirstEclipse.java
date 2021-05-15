c819f6608828b489e3bbe9317065fa63179620b1
swanstoz <swan_stoz@naver.com>
2021-05-15 14:01:26 +0900

    Add FirstEclipse.java

---
+++ JAVA/base/FirstEclipse.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class FirstEclipse {
+	/** JVM에 의해 자동 호출되는 특별한 메소드 */
+	public static void main(String[] args) {
+		System.out.print("이클립스로 만든");
+		System.out.println(" 첫번째 프로그램");
+	} //end of main
+} //end of class
