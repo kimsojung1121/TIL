52863a91978a04feceee1d8f44b0a4e288d05694
swanstoz <swan_stoz@naver.com>
2021-05-15 14:02:13 +0900

    Add Hello.java

---
+++ JAVA/base/Hello.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+/*
+교재 : 혼자 공부하는 자바 
+작성자 : 신용권
+*/
+
+public class Hello {
+//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}//end of main
+}//end of class
